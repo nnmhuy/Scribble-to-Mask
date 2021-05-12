9454448facc909230289b0b948784262e295f8de
Nguyễn Ngọc Minh Huy <nnmhuy@apcs.vn>
2021-05-12 10:53:30 +0700

    turn from interactive to static

--- interactive.py
+++ interactive.py
@@ -14,7 +14,7 @@ from util.tensor_util import pad_divide_by
 
 
 class InteractiveManager:
-    def __init__(self, model, image, mask):
+    def __init__(self, model, image, mask, p_srb, n_srb):
         self.model = model
 
         self.image = im_normalization(TF.to_tensor(image)).unsqueeze(0).cuda()
@@ -26,8 +26,8 @@ class InteractiveManager:
         self.last_mask = None
 
         # Positive and negative scribbles
-        self.p_srb = np.zeros((h, w), dtype=np.uint8)
-        self.n_srb = np.zeros((h, w), dtype=np.uint8)
+        self.p_srb = p_srb
+        self.n_srb = n_srb
 
         # Used for drawing
         self.pressed = False
@@ -35,29 +35,6 @@ class InteractiveManager:
         self.positive_mode = True
         self.need_update = True
 
-    def mouse_down(self, ex, ey):
-        self.last_ex = ex
-        self.last_ey = ey
-        self.pressed = True
-        if self.positive_mode:
-            cv2.circle(self.p_srb, (ex, ey), radius=3, color=(1), thickness=-1)
-        else:
-            cv2.circle(self.n_srb, (ex, ey), radius=3, color=(1), thickness=-1)
-        self.need_update = True
-
-    def mouse_move(self, ex, ey):
-        if not self.pressed:
-            return
-        if self.positive_mode:
-            cv2.line(self.p_srb, (self.last_ex, self.last_ey), (ex, ey), (1), thickness=3)
-        else:
-            cv2.line(self.n_srb, (self.last_ex, self.last_ey), (ex, ey), (1), thickness=3)
-        self.need_update = True
-        self.last_ex = ex
-        self.last_ey = ey
-
-    def mouse_up(self):
-        self.pressed = False
 
     def run_s2m(self):
         # Convert scribbles to tensors
@@ -99,7 +76,26 @@ parser = ArgumentParser()
 parser.add_argument('--image', default='ust_cat.jpg')
 parser.add_argument('--model', default='saves/s2m.pth')
 parser.add_argument('--mask', default=None)
+parser.add_argument('--p_srb', default=None)
+parser.add_argument('--n_srb', default=None)
+parser.add_argument('--output', default='output_mask.jpg')
 args = parser.parse_args()
+
+
+def comp_image(image, mask, p_srb, n_srb):
+    color_mask = np.zeros_like(image, dtype=np.uint8)
+    color_mask[:,:,2] = 1
+    if len(mask.shape) == 2:
+        mask = mask[:,:,None]
+    comp = (image*0.5 + color_mask*mask*0.5).astype(np.uint8)
+    comp[p_srb>0.5, :] = np.array([0, 255, 0], dtype=np.uint8)
+    comp[n_srb>0.5, :] = np.array([255, 0, 0], dtype=np.uint8)
+
+    return comp
+
+
+print('Usage: python interactive.py --image <image> --model <model> [Optional: --mask initial_mask]')
+
 
 # network stuff
 net = S2M()
@@ -115,75 +111,24 @@ if args.mask is None:
 else:
     mask = cv2.imread(args.mask, cv2.IMREAD_GRAYSCALE)
 
-manager = InteractiveManager(net, image, mask)
+if args.p_srb is None:
+    p_srb = np.zeros((h, w), dtype=np.uint8)
+else:
+    p_srb = cv2.imread(args.p_srb, cv2.IMREAD_GRAYSCALE)
+    p_srb[p_srb>=0.5] = 1
+    p_srb[p_srb<0.5] = 0
 
-def mouse_callback(event, x, y, *args):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        manager.mouse_down(x, y)
-    elif event == cv2.EVENT_LBUTTONUP:
-        manager.mouse_up()
-    elif event == cv2.EVENT_MBUTTONDOWN:
-        manager.positive_mode = not manager.positive_mode
-        if manager.positive_mode:
-            print('Entering positive scribble mode.')
-        else:
-            print('Entering negative scribble mode.')
 
-    # Draw
-    if event == cv2.EVENT_MOUSEMOVE:
-        manager.mouse_move(x, y)
+if args.n_srb is None:
+    n_srb = np.zeros((h, w), dtype=np.uint8)
+else:
+    n_srb = cv2.imread(args.n_srb, cv2.IMREAD_GRAYSCALE)
+    n_srb[n_srb>=0.5] = 1
+    n_srb[n_srb<0.5] = 0
 
-def comp_image(image, mask, p_srb, n_srb):
-    color_mask = np.zeros_like(image, dtype=np.uint8)
-    color_mask[:,:,2] = 1
-    if len(mask.shape) == 2:
-        mask = mask[:,:,None]
-    comp = (image*0.5 + color_mask*mask*0.5).astype(np.uint8)
-    comp[p_srb>0.5, :] = np.array([0, 255, 0], dtype=np.uint8)
-    comp[n_srb>0.5, :] = np.array([255, 0, 0], dtype=np.uint8)
+manager = InteractiveManager(net, image, mask, p_srb, n_srb)
 
-    return comp
 
-# OpenCV setup
-cv2.namedWindow('S2M demo')
-cv2.setMouseCallback('S2M demo', mouse_callback)
-
-print('Usage: python interactive.py --image <image> --model <model> [Optional: --mask initial_mask]')
-print('This GUI is rudimentary; the network is naively designed.')
-print('Mouse Left - Draw scribbles')
-print('Mouse middle key - Switch positive/negative')
-print('Key f - Commit changes, clear scribbles')
-print('Key r - Clear everything')
-print('Key d - Switch between overlay/mask view')
-print('Key s - Save masks into a temporary output folder (./output/)')
-
-display_comp = True
-while 1:
-    if manager.need_update:
-        np_mask = manager.run_s2m()
-        if display_comp:
-            display = comp_image(image, np_mask, manager.p_srb, manager.n_srb)
-        else:
-            display = np_mask
-        manager.need_update = False
-
-    cv2.imshow('S2M demo', display)
-
-    k = cv2.waitKey(1) & 0xFF
-    if k == ord('f'):
-        manager.commit()
-        manager.need_update = True
-    elif k == ord('s'):
-        print('saved')
-        os.makedirs('output', exist_ok=True)
-        cv2.imwrite('output/%s' % path.basename(args.mask), mask)
-    elif k == ord('d'):
-        display_comp = not display_comp
-        manager.need_update = True
-    elif k == ord('r'):
-        manager.clean_up()
-        manager.need_update = True
-    elif k == 27:
-        break
-
-cv2.destroyAllWindows()
+np_mask = manager.run_s2m()
+display = comp_image(image, np_mask, manager.p_srb, manager.n_srb)
+cv2.imwrite(args.output, display)
